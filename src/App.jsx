b3b503c99783b05ffb2497830f6908bdeac6befa
tstsukahara <9626666+tstsukahara@users.noreply.github.com>
2022-01-29 17:36:14 +0900

    react基本動作確認まで

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+/* eslint react-hooks/exhaustive-deps: off */
+import React, { useEffect, useState } from "react";
+import { ColorfulMessage } from "./components/ColorfulMessage";
+
+const App = () => {
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
+  const onClickCountup = () => {
+    setNum(num + 1);
+  };
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+  }, [num]);
+
+  return (
+    <>
+      <h1 style={{ color: "red" }}>こんにちは!</h1>
+      <ColorfulMessage color="blue">おげんきですか？</ColorfulMessage>
+      <ColorfulMessage color="pink">げんきです</ColorfulMessage>
+      <button onClick={onClickCountup}>カウントアップ</button>
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
+      <p>{num}</p>
+      {faceShowFlag && <p>( @_@)</p>}
+    </>
+  );
+};
+
+export default App;
